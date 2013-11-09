e730808c5e470b1b3a1b0298927e14e2f5666dcd
Young <young.guo@gmail.com>
2013-11-08 21:53:31 -0800

    got the combo boxes working

--- DAP2013/src/RegistrationPanel.java
+++ DAP2013/src/RegistrationPanel.java
@@ -9,7 +9,9 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
-public class RegistrationPanel extends JPanel{
+public class RegistrationPanel extends JPanel implements ActionListener{
+	private final JComboBox type;
+	private final JComboBox location;
 	public RegistrationPanel(){
 		this.setLayout(new GridLayout(8,2));
 		JButton b = new JButton("SEND");
@@ -18,16 +20,13 @@ public class RegistrationPanel extends JPanel{
 	    final JTextField first = new JTextField();
 	    final JTextField last = new JTextField();
 	    final JTextField chapter = new JTextField();
-	    b.addActionListener(new ActionListener(){
-	    @Override
-	    public void actionPerformed(ActionEvent arg0) {
-	    	System.out.println(first.getText() + " " + last.getText() + " " + chapter.getText());
-	    	}
-	    });
+
 		String [] types = {"Member","Advisor","Guest"};
 		String [] locations = {"DC","MN","LA"};
-		JComboBox type = new JComboBox(types);
-		JComboBox location = new JComboBox(locations);
+		type = new JComboBox(types);
+		type.addActionListener(this);
+		location = new JComboBox(locations);
+		location.addActionListener(this);
 	    this.add(new JLabel("Location:"));
 	    this.add(location);
 	    this.add(new JLabel("Type:"));
@@ -39,6 +38,15 @@ public class RegistrationPanel extends JPanel{
 		this.add(new JLabel("chapter"));
 		this.add(chapter);
 		this.add(b);
-
+	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource() == location){
+			String place = (String)location.getSelectedItem();
+			System.out.println(place);
+		}else if(e.getSource() == type){
+			String PERSONTYPE = (String)type.getSelectedItem();
+			System.out.println(PERSONTYPE);
+		}
 	}
 }
