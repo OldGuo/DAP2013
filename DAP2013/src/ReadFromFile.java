b8100c70a4e298686bb232a2b529760d5e16028f
Young <young.guo@gmail.com>
2013-11-18 17:28:15 -0800

    got tooltips working for the workshops

--- DAP2013/src/ReadFromFile.java
+++ DAP2013/src/ReadFromFile.java
@@ -5,15 +5,15 @@ public class ReadFromFile {
 	private static ArrayList<Workshop>workshops;
 	private static ArrayList<Participant>participants;
 	String[][]information;
-	
+
 	public ReadFromFile(){
 		workshops = new ArrayList<Workshop>();
 		participants = new ArrayList<Participant>();
 	}
 	public static ArrayList<Workshop> getWorkshops(){
 		workshops = new ArrayList<Workshop>();
-		//Scanner input = OpenFile.open("C:\\Users\\Young\\git\\FBLADAP2013\\DAP2013\\WORKSHOPS.txt"); //desktop
-		Scanner input = OpenFile.open("C:\\Users\\Young\\Documents\\GitHub\\DAP2013\\DAP2013\\WORKSHOPS.txt"); //laptop
+		Scanner input = OpenFile.open("C:\\Users\\Young\\git\\FBLADAP2013\\DAP2013\\WORKSHOPS.txt"); //desktop
+		//Scanner input = OpenFile.open("C:\\Users\\Young\\Documents\\GitHub\\DAP2013\\DAP2013\\WORKSHOPS.txt"); //laptop
 		ArrayList<String>info = new ArrayList<String>();
 		while(input.hasNextLine()){
 			info = new ArrayList<String>();
@@ -41,8 +41,8 @@ public class ReadFromFile {
 	}
 	public static ArrayList<Participant> getParticipants(){
 		participants = new ArrayList<Participant>();
-		//Scanner input = OpenFile.open("C:\\Users\\Young\\git\\FBLADAP2013\\DAP2013\\PARTICIPANTS.txt"); //desktop
-		Scanner input = OpenFile.open("C:\\Users\\Young\\Documents\\GitHub\\DAP2013\\DAP2013\\PARTICIPANTS.txt"); //laptop
+		Scanner input = OpenFile.open("C:\\Users\\Young\\git\\FBLADAP2013\\DAP2013\\PARTICIPANTS.txt"); //desktop
+		//Scanner input = OpenFile.open("C:\\Users\\Young\\Documents\\GitHub\\DAP2013\\DAP2013\\PARTICIPANTS.txt"); //laptop
 		ArrayList<String>info = new ArrayList<String>();
 		while(input.hasNextLine()){
 			info = new ArrayList<String>();
--- DAP2013/src/WorkshopPanel.java
+++ DAP2013/src/WorkshopPanel.java
@@ -1,25 +1,37 @@
+import java.awt.Component;
 import java.util.ArrayList;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 
 public class WorkshopPanel extends JPanel{
-	private JScrollPane scrollPane;
+	private final JScrollPane scrollPane;
 	private Object[]data;
-	private DefaultTableModel model;
-	private JTable table;
-	
+	private final DefaultTableModel model;
+	private final JTable table;
+
 	public WorkshopPanel(){
 		String [] columnNames = {"Location","Title","Description","Date","Time"};
-		table = new JTable(new DefaultTableModel());
-        
+		table = new JTable(){
+			 @Override
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+			        Component c = super.prepareRenderer(renderer, row, column);
+			        if (c instanceof JComponent) {
+			            JComponent jc = (JComponent) c;
+			            jc.setToolTipText(getValueAt(row, column).toString());
+			        }
+			        return c;
+			    }
+		};
         model = (DefaultTableModel) table.getModel();
         model.setColumnIdentifiers(columnNames);
 		loadData();
-        
+
         scrollPane = new JScrollPane(table);
         this.add(scrollPane);
 	}
@@ -34,6 +46,12 @@ public class WorkshopPanel extends JPanel{
 			data[3]= workshops.get(i).getDate().toString();
 			data[4]= workshops.get(i).getTime().toString();
 			model.addRow(data);
+		}
+	}
+	public class MyTableModel extends DefaultTableModel {
+		@Override
+		public boolean isCellEditable(int row, int column) {
+			return false;
 		}
 	}
 }
