da402205d66b97db323062e88b9912f27c85720f
Young <young.guo@gmail.com>
2013-11-18 18:55:23 -0800

    trying tooltip things, but it seems like it might not work

--- DAP2013/src/WorkshopPanel.java
+++ DAP2013/src/WorkshopPanel.java
@@ -1,10 +1,13 @@
 import java.awt.Component;
+import java.awt.Dimension;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JToolTip;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
@@ -12,46 +15,59 @@ import javax.swing.table.TableCellRenderer;
 public class WorkshopPanel extends JPanel{
 	private final JScrollPane scrollPane;
 	private Object[]data;
-	private final DefaultTableModel model;
-	private final JTable table;
+	private DefaultTableModel model;
+	private JTable table;
+	private ArrayList<Workshop>workshops;
 
 	public WorkshopPanel(){
-		String [] columnNames = {"Location","Title","Description","Date","Time"};
-		table = new JTable(){
-			 @Override
-			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
-			        Component c = super.prepareRenderer(renderer, row, column);
-			        if (c instanceof JComponent) {
-			            JComponent jc = (JComponent) c;
-			            jc.setToolTipText(getValueAt(row, column).toString());
-			        }
-			        return c;
-			    }
-		};
-        model = (DefaultTableModel) table.getModel();
-        model.setColumnIdentifiers(columnNames);
+		JLabel label = new JLabel("Mouse over for descriptions");
 		loadData();
-
+		createTable();
         scrollPane = new JScrollPane(table);
+        scrollPane.setPreferredSize(new Dimension(900,450));
+        this.add(label);
         this.add(scrollPane);
 	}
 	public void loadData(){
 		ReadFromFile r = new ReadFromFile();
-		ArrayList<Workshop>workshops = r.getWorkshops();
-		data = new String[5];
+		workshops = r.getWorkshops();
+	}
+	public void createTable(){
+		String [] columnNames = {"Location","Title","Date","Time"};
+		table = new MyTableModel();
+        model = (DefaultTableModel) table.getModel();
+        model.setColumnIdentifiers(columnNames);
+		data = new String[4];
 		for(int i = 0; i < workshops.size();i++){
 			data[0]= workshops.get(i).getCode().toString();
 			data[1]= workshops.get(i).getTitle().toString();
-			data[2]= workshops.get(i).getDescrip().toString();
-			data[3]= workshops.get(i).getDate().toString();
-			data[4]= workshops.get(i).getTime().toString();
+			data[2]= workshops.get(i).getDate().toString();
+			data[3]= workshops.get(i).getTime().toString();
 			model.addRow(data);
 		}
+		table.getColumnModel().getColumn(0).setPreferredWidth(75);
+		table.getColumnModel().getColumn(1).setPreferredWidth(1000);
+		table.getColumnModel().getColumn(2).setPreferredWidth(75);
+		table.getColumnModel().getColumn(3).setPreferredWidth(75);
 	}
-	public class MyTableModel extends DefaultTableModel {
+	public class MyTableModel extends JTable {
 		@Override
 		public boolean isCellEditable(int row, int column) {
 			return false;
+		}
+		@Override
+		public JToolTip createToolTip(){
+			JToolTip tip = new JToolTip();
+			return tip;
+		}
+		@Override
+		public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+			Component c = super.prepareRenderer(renderer, row, column);
+			if (c instanceof JComponent) {
+				JComponent jc = (JComponent) c;
+				jc.setToolTipText(MultiLineTooltips.splitToolTip(workshops.get(row).getDescrip()));
+			}
+			return c;
 		}
 	}
 }
