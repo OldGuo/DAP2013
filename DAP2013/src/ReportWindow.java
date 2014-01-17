19c3fddf58be3890597e5b24fe3f418bca5bc03e
Young <young.guo@gmail.com>
2014-01-16 16:07:56 -0800

    still working on reports

--- DAP2013/src/ReportWindow.java
+++ DAP2013/src/ReportWindow.java
@@ -27,9 +27,19 @@ public class ReportWindow extends JDialog{
 			tabbedPane.add(MN,"MN");
 			
 		}else if(reportType.equals("Participant List for Each Workshop")){
-			JPanel workshops = new JPanel();
+			JPanel registrations = new JPanel();
 			
-			tabbedPane.add(workshops,"Workshop");
+			ReadFromFile read = new ReadFromFile("WORKSHOPS");
+			ArrayList<Object>workshops = read.getData();
+			
+			for(int i = 0; i < workshops.size(); i++){
+				Workshop w = (Workshop)workshops.get(i);
+				JPanel temp = new JPanel();
+				tabbedPane.add(w.getTitle(),temp);
+			}
+			
+			
+			tabbedPane.add(registrations,"Workshop");
 		}else if(reportType.equals("Participant Schedule")){
 			ReadFromFile read = new ReadFromFile("PARTICIPANTS");
 			ArrayList<Object>participants = read.getData();
